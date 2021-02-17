18abc97df428d96ffd67a9107b6c3f0425ac2e24
Ines Radic <inesradic12314@gmail.com>
2021-02-16 19:03:37 -0800

    insert queries

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Whiskey King Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Luger Burger', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Chargrilled Burger', false);
