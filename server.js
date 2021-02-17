011ff8eeb0b1e424c3ebf8f6a1615d7a9cb42180
Ines Radic <inesradic12314@gmail.com>
2021-02-16 18:48:09 -0800

    addseeds file and notes to server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+// initialize express app
+
+// define a port
+
+// define routes
+    // get route to get all burgers
+    // post route to post tweet
+
+// app listening function
