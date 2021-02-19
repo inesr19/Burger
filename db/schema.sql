7cbc55b547c0bdff50efe0e1724a6378af616662
Ines Radic <inesradic12314@gmail.com>
2021-02-18 21:51:20 -0800

    add default

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(30) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY(id)
 );
