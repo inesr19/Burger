8f0c5a78dcc30f9f2ec57635ee7c3b47c550afe4
Ines Radic <inesradic12314@gmail.com>
2021-02-18 22:21:59 -0800

    add get and post functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,5 +2,28 @@ const express = require('express');
 const burger = require('../models/burger');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.redirect('/index');
+});
 
+router.get('/index', (req, res) => {
+   burger.selectAll((data) => {
+       
+       res.render('index', { burgers: data});
+   });
+});
+
+router.post('/', (req, res) => {
+   burger.insertOne(req.body.burger, () => {
+       res.redirect('/index');
+   });
+});
+
+router.post('/:id', (req, res) => {
+   burger.updateOne(req.params.id, () => {
+       res.redirect('/index')
+   });
+});
+
+module.exports = router;
 
